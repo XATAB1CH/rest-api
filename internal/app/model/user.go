84f0c6ed075c8a1004aff36bf93c21b56966a84b
XATAB1CH <demidovanton34@gmail.com>
2024-03-18 19:33:58 +0300

    Todo

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,8 +1,57 @@
 package model
 
-// Наш user
+import (
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var validate *validator.Validate
+
+// User
 type User struct {
 	Id                int
-	Email             string
+	Email             string `validate:"email"`
+	Password          string `validate:"custom"`
 	EncryptedPassword string
+}
+
+func customFunc(fl validator.FieldLevel) bool {
+	u := fl.Field().Interface().(User)
+
+	if u.EncryptedPassword != "" {
+		return true
+	}
+
+	return false
+}
+
+// Validate
+func (u *User) Validate() error {
+	validate.RegisterValidation("custom", customFunc)
+	validate = validator.New(validator.WithRequiredStructEnabled())
+	return validate.Struct(u)
+}
+
+// BeforeCreate
+func (u *User) BeforeCreate() error {
+
+	if len(u.Password) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return err
+		}
+
+		u.EncryptedPassword = enc
+	}
+	return nil
+}
+
+// Encrypt password
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
--- internal/store/userrepository.go
+++ internal/store/userrepository.go
@@ -2,13 +2,25 @@ package store
 
 import "github.com/XATAB1CH/rest-api/internal/app/model"
 
-// UserRepository ...
+// UserRepository
 type UserRepository struct {
 	store *Store
 }
 
 // Создание нового пользователя
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+
+	// Проверка на валидность
+	if err := u.Validate(); err != nil {
+		return nil, err
+	}
+
+	// Хэширование пароля
+	if err := u.BeforeCreate(); err != nil {
+		return nil, err
+	}
+
+	// Запись пользователя в БД
 	if err := r.store.db.QueryRow(
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		u.Email,
@@ -20,7 +32,7 @@ func (r *UserRepository) Create(u *model.User) (*model.User, error) {
 	return u, nil
 }
 
-// FindByEmail returns a user by email
+// FindByEmail
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 	u := &model.User{}
 	if err := r.store.db.QueryRow(
